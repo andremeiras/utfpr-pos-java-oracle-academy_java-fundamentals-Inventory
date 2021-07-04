bdb125d35f5d72a1dd736b6ce41d2b5c13b94d55
André Meira <andremeirati@gmail.com>
2021-07-04 11:51:58 -0300

    Feito até exercício 3 da sessão 6

--- src/inventario/ProductTester.java
+++ src/inventario/ProductTester.java
@@ -1,5 +1,7 @@
 package inventario;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ProductTester {
@@ -9,90 +11,48 @@ public class ProductTester {
 		// Variável que irá solicitar entrada de dados via teclado
 		Scanner in = new Scanner(System.in);
 
-		int maxSize = 0;
+		int maxSize = -1;
 
 		// variáveis temporárias para receber os valores digitados do produto
 		int tempNumber;
 		String tempName;
 		int tempQty;
 		double tempPrice;
-
 		do {
 			try {
-				System.out.println(
-						"Insira o número de produtos que gostaria de adicionar\nInsira 0 (zero) se não quiser adicionar produtos:");
+				System.out.println("Insira o número de produtos que gostaria de adicionar");
+				System.out.println("Insira 0 (zero) se não quiser adicionar produtos:");
 				maxSize = in.nextInt();
+				
 				if (maxSize < 0) {
-					System.out.println("Valor incorreto inserido.");
-				} else {
-					for (int i = 0; i <= maxSize; i++) {
-						System.out.print("Digite o número do produto: ");
+					System.out.println("Valor inválido!");
+				} else if (maxSize > 0) {
+					do {
+						System.out.println("[Produto nº " + maxSize + "]");
+						System.out.print("Número/Código do produto: ");
 						tempNumber = in.nextInt();
-
-						System.out.print("Digite o nome do produto: ");
+						System.out.print("Nome do produto: ");
 						tempName = in.next();
-
-						System.out.print("Digite a quantidade de produtos em estoque: ");
+						System.out.print("Quantidade: ");
 						tempQty = in.nextInt();
-
-						System.out.print("Digite o valor unitário do produto: ");
+						System.out.print("Valor: R$ ");
 						tempPrice = in.nextDouble();
-						i++;
-					}
+						System.out.println();
+						maxSize--;
+					} while (maxSize > 0);
+				} else if (maxSize == 0) {
+					System.out.println("Não há produtos!");
+				} else {
+					System.out.println("Saindo do programa!");
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
+				System.out.println("Tipo incorreto de dados inserido!");
+				in.close();
+				break;
 			}
-		} while (maxSize != 0);
-
-		/*
-		 * CADASTRO p1
-		 */
-		System.out.println("[CADASTRO p1]");
-
-		// Solicitando entrada de dados do usuário que irá armazenar em cada variável
-		System.out.print("Digite o número do produto: ");
-		tempNumber = in.nextInt();
-
-		System.out.print("Digite o nome do produto: ");
-		tempName = in.next();
-
-		System.out.print("Digite a quantidade de produtos em estoque: ");
-		tempQty = in.nextInt();
-
-		System.out.print("Digite o valor unitário do produto: ");
-		tempPrice = in.nextDouble();
-
-		// Instancia de Produto passando os parâmetros digitados pelo usuário para o
-		// construtor
-		Produto p1 = new Produto(tempNumber, tempName, tempQty, tempPrice);
-		System.out.println();
-		// FIM CADASTRO p1
-
-		/*
-		 * CADASTRO p2
-		 */
-		System.out.println("[CADASTRO p2]");
-		// Solicitando entrada de dados do usuário que irá armazenar em cada variável
-		System.out.print("Digite o número do produto: ");
-		tempNumber = in.nextInt();
-
-		System.out.print("Digite o nome do produto: ");
-		tempName = in.next();
-
-		System.out.print("Digite a quantidade de produtos em estoque: ");
-		tempQty = in.nextInt();
-
-		System.out.print("Digite o valor unitário do produto: ");
-		tempPrice = in.nextDouble();
-
-		in.close();
-
-		// Instancia de Produto passando os parâmetros digitados pelo usuário para o
-		// construtor
-		Produto p2 = new Produto(tempNumber, tempName, tempQty, tempPrice);
-		// FIM CADASTRO p2
-
+		} while (maxSize < 0);
+		System.out.println("Fim do programa!");
 	}
 
 }
